6cc085253c9ed2aa5b93ea348c3af91294090a12
Krzysztof Radziszewski <krzysiu.piotr.r@gmail.com>
2024-06-30 19:11:51 +0200

    guzik

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const questions = [
-    "Wymień cechy “kina jako sztuki dla umysłu” w teorii Munsterberga.",
-    "Na czym polega językowa natura kina wg. Epsteina?",
-    "Opisz pojęcie fotogenii u Epsteina.",
-    "Wskaż różnice między kinem a teatrem, na które zwraca uwagę Bela Balazs.",
-    "Co jest źródłem języka sztuki filmowej wg. Balazsa? Wymień konkretne wyróżniki z jego teorii.",
-    "Czym charakteryzowała się filmologia francuska?",
-    "Widmo, a obraz myślowy – opisz terminy i wskaż ich autora.",
-    "Na czym polega projekcja-identyfikacja, jakie są fazy projekcji i dlaczego kino działa doskonale jako machina do ich produkcji?",
-    "Wymień i opisz różne podejścia w teoriach autorskich.",
-    "Porównaj koncepcję znaku u de Saussure’a i Peirce’a.",
-    "Czym jest strukturalizm, a czym jest semiotyka?",
-    "Omów teorię znaczenia u Rolanda Barthesa.",
-    "Wskaż elementy teorii znaczenia w filmie Christiana Metza.",
-    "Na czym polega hermeneutyka – opisz, wskaż jej badacza i wymień elementy jego koncepcji.",
-    "Opisz różnice w definiowaniu “psyche” od 1900 r. i od 1920 r.",
-    "Czym są archetyp i jak działa marzenie senne u Junga?",
-    "Co to jest melancholia?",
-    "Jakie sfery i porządki wyróżnia Lacan? Czym wg. niego jest “nieświadome”?",
-    "Na czym polega faza zwierciadła i czym jest “Realne”? Kto jest autorem tych definicji?",
-    "Czym jest “regresja” i kto o niej pisał?",
-    "W jaki sposób jaskinia Platona mogłaby być uznana za zapowiedź kina?",
-    "Porównaj kino i sen oraz opisz różnicę między nimi (wg. Metza).",
-    "Wskaż autorkę i opisz koncepcję zawartą w publikacji “Przyjemność wizualna i kino narracyjne.",
-    "Na czym polega paradoks związany z obrazem kobiety w koncepcjach feministycznych?",
-    "Wskaż i krótko scharakteryzuj cztery fale feminizmu.",
-    "Gender studies, queer studies – po co i dlaczego?",
-    "Na co zwraca uwagę J. Butler w swojej najważniejszej publikacji (podaj tytuł)?",
-    "Postmodernizm – skąd się wziął i jakie są jego cechy?",
-    "Brian McHale i Frederic Jameson – jak rozpatrywali postmodernizm (wskaż podobieństwa/opozycje).",
-    "Czego dotyczy Deleuzowski nomadyzm?",
-    "Opisz krótko koncepcję symulakrów.",
-    "Film i Odbiór w teorii neoformatywno-kognitywnej Bordwella – co je charakteryzuje?",
-    "Czym jest forma w teorii Bordwella?",
-    "Z jakich faz składa się aktywność odbiorczo w teorii neoformatywno-kognitywnej?",
-    "Jakie zasady rządzą formą filmową w teorii neoformatywno-kognitywnej?",
-    "Opisz do czego dążą badania postokolonialistyczne, wskaż badaczy oraz ich koncepcje.",
-    "Czym charakteryzuje się zmiana w pejzażu współczesnych badań filmoznawczych? Co aktualnie staje się przedmiotem tych badań?",
-    "Z jakich etapów składa się dystrybucja filmowa?",
-    "Wskaż film skutecznie obrazujący założenia ekokrytyki i opisz jego najważniejsze cechy z tym związane.",
-    "Rozwiń pojęcie fenomenologia i wskaż najważniejsze terminy z nią związane.",
-    "Opisz pogląd Maurice’a Merleau-Ponty’ego na percepcję.",
-    "Czym jest somaestetyka?",
-    "Co interesuje neuroestetyków?",
-    "Kto jest ojcem neurofilomologii i czym zajmuje się ta dziedzina?",
-    "Opisz jaki wpływ na teorię filmumają neurony lustrzane?",
-    "Kora oczodołowo-czołowa i hipokamp – jakie procesy istotne dla teorii neuroestetycznych i neurofilmologicznych zachodzą w tych ośrodkach mózgowych?"
-];
+      "Wymień cechy “kina jako sztuki dla umysłu” w teorii Munsterberga.",
+      "Na czym polega językowa natura kina wg. Epsteina?",
+      "Opisz pojęcie fotogenii u Epsteina.",
+      "Wskaż różnice między kinem a teatrem, na które zwraca uwagę Bela Balazs.",
+      "Co jest źródłem języka sztuki filmowej wg. Balazsa? Wymień konkretne wyróżniki z jego teorii.",
+      "Czym charakteryzowała się filmologia francuska?",
+      "Widmo, a obraz myślowy – opisz terminy i wskaż ich autora.",
+      "Na czym polega projekcja-identyfikacja, jakie są fazy projekcji i dlaczego kino działa doskonale jako machina do ich produkcji?",
+      "Wymień i opisz różne podejścia w teoriach autorskich.",
+      "Porównaj koncepcję znaku u de Saussure’a i Peirce’a.",
+      "Czym jest strukturalizm, a czym jest semiotyka?",
+      "Omów teorię znaczenia u Rolanda Barthesa.",
+      "Wskaż elementy teorii znaczenia w filmie Christiana Metza.",
+      "Na czym polega hermeneutyka – opisz, wskaż jej badacza i wymień elementy jego koncepcji.",
+      "Opisz różnice w definiowaniu “psyche” od 1900 r. i od 1920 r.",
+      "Czym są archetyp i jak działa marzenie senne u Junga?",
+      "Co to jest melancholia?",
+      "Jakie sfery i porządki wyróżnia Lacan? Czym wg. niego jest “nieświadome”?",
+      "Na czym polega faza zwierciadła i czym jest “Realne”? Kto jest autorem tych definicji?",
+      "Czym jest “regresja” i kto o niej pisał?",
+      "W jaki sposób jaskinia Platona mogłaby być uznana za zapowiedź kina?",
+      "Porównaj kino i sen oraz opisz różnicę między nimi (wg. Metza).",
+      "Wskaż autorkę i opisz koncepcję zawartą w publikacji “Przyjemność wizualna i kino narracyjne.",
+      "Na czym polega paradoks związany z obrazem kobiety w koncepcjach feministycznych?",
+      "Wskaż i krótko scharakteryzuj cztery fale feminizmu.",
+      "Gender studies, queer studies – po co i dlaczego?",
+      "Na co zwraca uwagę J. Butler w swojej najważniejszej publikacji (podaj tytuł)?",
+      "Postmodernizm – skąd się wziął i jakie są jego cechy?",
+      "Brian McHale i Frederic Jameson – jak rozpatrywali postmodernizm (wskaż podobieństwa/opozycje).",
+      "Czego dotyczy Deleuzowski nomadyzm?",
+      "Opisz krótko koncepcję symulakrów.",
+      "Film i Odbiór w teorii neoformatywno-kognitywnej Bordwella – co je charakteryzuje?",
+      "Czym jest forma w teorii Bordwella?",
+      "Z jakich faz składa się aktywność odbiorczo w teorii neoformatywno-kognitywnej?",
+      "Jakie zasady rządzą formą filmową w teorii neoformatywno-kognitywnej?",
+      "Opisz do czego dążą badania postokolonialistyczne, wskaż badaczy oraz ich koncepcje.",
+      "Czym charakteryzuje się zmiana w pejzażu współczesnych badań filmoznawczych? Co aktualnie staje się przedmiotem tych badań?",
+      "Z jakich etapów składa się dystrybucja filmowa?",
+      "Wskaż film skutecznie obrazujący założenia ekokrytyki i opisz jego najważniejsze cechy z tym związane.",
+      "Rozwiń pojęcie fenomenologia i wskaż najważniejsze terminy z nią związane.",
+      "Opisz pogląd Maurice’a Merleau-Ponty’ego na percepcję.",
+      "Czym jest somaestetyka?",
+      "Co interesuje neuroestetyków?",
+      "Kto jest ojcem neurofilomologii i czym zajmuje się ta dziedzina?",
+      "Opisz jaki wpływ na teorię filmumają neurony lustrzane?",
+      "Kora oczodołowo-czołowa i hipokamp – jakie procesy istotne dla teorii neuroestetycznych i neurofilmologicznych zachodzą w tych ośrodkach mózgowych?"
+  ];
 
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  const randomQuestion = questions[randomIndex];
+  function displayRandomQuestion() {
+      const randomIndex = Math.floor(Math.random() * questions.length);
+      const randomQuestion = questions[randomIndex];
+      document.getElementById('question').textContent = randomQuestion;
+  }
 
-  document.getElementById('question').textContent = randomQuestion;
+  document.getElementById('new-question-btn').addEventListener('click', displayRandomQuestion);
+
+  // Display a question when the page loads
+  displayRandomQuestion();
 });
